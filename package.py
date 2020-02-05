1a9ce8d376e2fd6ad124b82b8444b33b71fd1941
Scott Gibbs <scott@gibbz.biz>
2020-02-05 13:15:28 -0600

    Fixed issues with the package script which would error if the color description file directory existed.

--- package.py
+++ package.py
@@ -1,6 +1,7 @@
-from sys import platform
-from shutil import copytree
 import PyInstaller.__main__
+from os.path import exists
+from shutil import copytree, rmtree
+from sys import platform
 
 is_windows = platform == 'win32'
 sep = ';' if is_windows else ':' # Platform specific data separator
@@ -18,4 +19,7 @@ PyInstaller.__main__.run([
     'main.py',
 ])
 
-copytree('colors', f'{dist_folder}\\color_files')
+
+color_file_dir = f'{dist_folder}\\color_files'
+if exists(color_file_dir): rmtree(color_file_dir)
+copytree('colors', color_file_dir)
